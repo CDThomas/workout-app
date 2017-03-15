9308ada4ab50c5ceb730a66e0d2e7546ce90070e
CDThomas <cdthomas.92@gmail.com>
2017-03-15 18:58:51 -0400

    Add exercise model. Configure RSpec

--- client/app/bundles/App/components/App.js
+++ client/app/bundles/App/components/App.js
@@ -11,9 +11,6 @@ export default class App extends React.Component {
    */
   constructor(props, _railsContext) {
     super(props);
-
-    // How to set initial state in ES6 class syntax
-    // https://facebook.github.io/react/docs/reusable-components.html#es6-classes
     this.state = { name: this.props.name };
   }
 

efc65443b873f5c15488938b0d8d534ae7592e57
CDThomas <cdthomas.92@gmail.com>
2017-03-16 17:14:28 -0400

    Add simple seed file for exercises

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+exercise_names = [
+  "Squat",
+  "Leg press",
+  "Lunge",
+  "Deadlift",
+  "Leg extension",
+  "Leg curl",
+  "Standing calf raise",
+  "Seated calf raise",
+  "Hip adductor",
+  "Bench press",
+  "Chest fly",
+  "Push-up",
+  "Pulldown",
+  "Pull-up",
+  "Bent-over row",
+  "Upright row",
+  "Shoulder press",
+  "Shoulder fly",
+  "Lateral raise",
+  "Shoulder shrug",
+  "Pushdown",
+  "Triceps extension",
+  "Biceps curl",
+  "Crunch",
+  "Russian twist",
+  "Leg raise",
+  "Back extension"
+]
+
+exercise_names.each do |name|
+  Exercise.create(name: name)
+end

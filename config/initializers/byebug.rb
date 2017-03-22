8042b0a79b079632ede2ff0a7e0a06ed9be39325
CDThomas <cdthomas.92@gmail.com>
2017-03-21 21:33:32 -0400

    Add silly byebug intitializer

---
+++ config/initializers/byebug.rb
@@ -0,0 +1,9 @@
+## Uncomment below for debugging with byebug.
+## This is jank, but works until I find a better solution
+
+# require 'byebug/core'
+
+# if Rails.env.development?
+#   Byebug.wait_connection = true
+#   Byebug.start_server 'localhost', ENV.fetch("BYEBUG_SERVER_PORT", 3031).to_i
+# end
